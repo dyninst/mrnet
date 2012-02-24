a1519b72808963503481bd7c323c2e8a81dbb430
Michael Brim <mjbrim@grilled.cs.wisc.edu>
2012-02-24 11:49:54 -0600

    also annoying in lightweight

--- src/lightweight/utils_lightweight.c
+++ src/lightweight/utils_lightweight.c
@@ -107,7 +107,7 @@ int mrn_printf( const char *file, int line, const char * func,
 
     if (file) {
         base_file = GetFilename(file);
-        fprintf( f, "%ld.%06ld: %s[%d] %s() - ",
+        fprintf( f, "%ld.%06ld: %s[%d] %s - ",
                  tv.tv_sec-MRN_RELEASE_DATE_SECS, 
                  tv.tv_usec, 
                  base_file,
