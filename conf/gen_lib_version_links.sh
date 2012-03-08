f899c0f32903406dd4333358e0ac8780712efd7a
Michael Brim <mjbrim@grilled.cs.wisc.edu>
2012-03-07 21:02:50 -0600

    better library version links

---
+++ conf/gen_lib_version_links.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ $# -ne 5 ]; then
+    echo "ERROR: Usage - $0 libdir libname major minor revision"
+    exit 1
+fi
+
+script_dir=`dirname $0`
+
+libdir=$1
+libname=$2
+vmajor=$3
+vminor=$4
+vrev=$5
+
+# link .so
+orig_libso="${libname}.so.${vmajor}.${vminor}.${vrev}"
+major_libso="${libname}.so.${vmajor}"
+libso=${libname}.so
+if [ -e $libdir/$orig_libso ]; then
+    ${script_dir}/install_link.sh $orig_libso $libdir $major_libso
+    ${script_dir}/install_link.sh $major_libso $libdir $libso
+fi
+
+
+# link .a
+orig_liba="${libname}.a.${vmajor}.${vminor}.${vrev}"
+liba=${libname}.a
+if [ -e $libdir/$orig_liba ]; then
+    ${script_dir}/install_link.sh $orig_liba $libdir $liba
+fi
+
---
+++ conf/install_link.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+if [ $# -ne 3 ]; then
+  echo "ERROR: Usage - $0 target install-dir link-name"
+  exit 1
+fi
+
+target=$1
+idir=$2
+lnk=$3
+
+echo "Installing symlink to $target with name $lnk in directory $idir"
+
+if [ ! -d $idir ]; then
+  echo "ERROR: $0 - installation directory $idir not found"
+  exit 1
+fi
+
+pushd $idir > /dev/null 2>&1
+
+if [ ! -e $target ]; then
+  echo "ERROR: $0 - target $idir/$target not found"
+  exit 1
+fi
+
+if [ -e $lnk ]; then
+  echo "- removing existing file $idir/$lnk"
+  /bin/rm -f $lnk
+fi
+
+ln_cmd=`which ln`
+if [ $? -eq 0 ]; then
+  $ln_cmd -s $target $lnk
+  result=$?
+else
+  echo "ERROR: $0 - failed to create symlink"
+  result=1
+fi
+
+popd > /dev/null 2>&1
+
+exit $result
