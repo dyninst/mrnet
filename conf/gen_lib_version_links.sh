00c621b5c980ca6b2afdd3262bfb9c0d343cef2d
Benjamin Welton <welton@aemilia.cs.wisc.edu>
2013-02-13 11:20:51 -0600

    Added generation of <libraryname><version>.so symlink

--- conf/gen_lib_version_links.sh
+++ conf/gen_lib_version_links.sh
@@ -16,10 +16,12 @@ vrev=$5
 # link .so
 orig_libso="${libname}.so.${vmajor}.${vminor}.${vrev}"
 major_libso="${libname}.so.${vmajor}"
+versioned_libso="${libname}.${vmajor}.${vminor}.${vrev}.so"
 libso=${libname}.so
 if [ -e $libdir/$orig_libso ]; then
     ${script_dir}/install_link.sh $orig_libso $libdir $major_libso
     ${script_dir}/install_link.sh $major_libso $libdir $libso
+    ${script_dir}/install_link.sh $orig_libso $libdir $versioned_libso
 fi
 
 
