94bedf8aa2ebad925e9ab934ed7a5e6bf29195a9
Michael J. Brim <brimmj@ornl.gov>
2013-05-02 11:36:43 -0400

    fix lightweight XPlat_NetUtils_GetLocalHostName on Cray

--- xplat/src/lightweight/NetUtils-unix.c
+++ xplat/src/lightweight/NetUtils-unix.c
@@ -15,30 +15,28 @@ int XPlat_NetUtils_GetLocalHostName(char* this_host)
 {
 #if defined(arch_crayxt)
     // on the XT, the node number is available in /proc/cray_xt/nid
-    const char* filename = "/proc/cray_xt/nid";
+    char host[16];
+    char* nidstr;
     FILE* f;
+    const char* filename = "/proc/cray_xt/nid";
+    unsigned nid;
+    int rc = 0;
     f = fopen(filename, "r");
-    uint32_t nid;
-    fread(&nid, sizeof(uint32_t), 1, f);
-    fclose(f);
-
-    // move nid into string
-    char* nid_str = (char*)malloc(sizeof(char)*6);
-    assert(nid_str);
-    sprintf(nid_str, "%05d", nid);
-
-    char* nidStr = (char*)malloc(sizeof(char)*9);
-    assert(nidStr);
-    strcat(nidStr, "nid");
-    strcat(nidStr, nid_str);
-
-    strncpy(this_host, nidStr, XPLAT_MAX_HOSTNAME_LEN);
+    if( NULL != f ) {
+        if( 1 == fscanf(f, "%u", &nid) ) {
+            sprintf( host, "nid%05u", nid );
+            strcpy( this_host, host );
+        }
+        else rc = 1;
+        fclose(f);
+    }
+    else rc = 1;
+    return rc;
 #else
     char host[XPLAT_MAX_HOSTNAME_LEN];
     gethostname(host, (size_t)XPLAT_MAX_HOSTNAME_LEN);
     host[XPLAT_MAX_HOSTNAME_LEN-1] = '\0';
     strncpy(this_host, host, (size_t)XPLAT_MAX_HOSTNAME_LEN);
-#endif
-
     return 0;
+#endif
 }
