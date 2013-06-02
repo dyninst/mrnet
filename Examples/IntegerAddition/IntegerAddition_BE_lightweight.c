9547633d35d231163e7d719bf0cffa5969e302eb
Evan Samanas <evan.samanas@gmail.com>
2013-06-02 05:30:22 -0500

    Fix for hang in IntegerAddition example
    In the event that the flush call in the PROT_SUM protocol of the
    example failed in the BE, a PROT_EXIT message would not be sent,
    whether a message was received requesting it or not.  This caused
    the FE to hang waiting for all PROT_EXIT messages.
    
    Currently, if the FE fails, this change causes the BEs to fail to
    exit instead.  This problem will be targeted with the new shutdown protocol.

--- Examples/IntegerAddition/IntegerAddition_BE_lightweight.c
+++ Examples/IntegerAddition/IntegerAddition_BE_lightweight.c
@@ -37,13 +37,12 @@ int main(int argc, char **argv)
             for( i=0; i<num_iters; i++ ) {
                 fprintf( stdout, "BE: Sending wave %u ...\n", i);
                 if( Stream_send(stream, tag, "%d", recv_val*i) == -1 ){
-                    fprintf(stderr, "BE: stream::send(%%d) failure\n");
+                    fprintf(stderr, "BE: stream::send(%%d) failure in PROT_SUM\n");
                     tag = PROT_EXIT;
                     break;
                 }
                 if( Stream_flush(stream ) == -1 ){
-                    fprintf(stderr, "BE: stream::flush() failure\n");
-                    tag = PROT_EXIT;
+                    fprintf(stderr, "BE: stream::flush() failure in PROT_SUM\n");
                     break;
                 }
                 fflush(stdout);
@@ -52,12 +51,12 @@ int main(int argc, char **argv)
             break;
 
         case PROT_EXIT:
-	    if( Stream_send(stream, tag, "%d", 0) == -1 ) {
-                fprintf(stderr, "BE: stream::send(%%s) failure\n");
+            if( Stream_send(stream, tag, "%d", 0) == -1 ) {
+                fprintf(stderr, "BE: stream::send(%%s) failure in PROT_EXIT\n");
                 break;
             }
             if( Stream_flush(stream ) == -1 ) {
-                fprintf(stderr, "BE: stream::flush() failure\n");
+                fprintf(stderr, "BE: stream::flush() failure in PROT_EXIT\n");
             }
             break;
 
